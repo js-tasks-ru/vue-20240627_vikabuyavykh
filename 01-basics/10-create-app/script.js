8d931ee33c8a58a2ffbfd854da50ccd11ddbd173
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-07-16 21:25:33 +0300

    both basics tasks are done

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,20 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    const date = new Date().toLocaleString('en', { dateStyle: 'long' })
+    return {
+      date,
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ date }}</div>
+  `,
+})
+
+const app = createApp(App)
+
+app.mount('#app')
